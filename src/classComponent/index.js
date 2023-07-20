8461e75cf2cd68dc9f23c1edce80a932a5aaef1f
Shushan04 <ash118817@gmail.com>
2023-07-20 12:51:25 +0400

    set up folders

---
+++ src/classComponent/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class MyClassComponent extends React.Component {
+    constructor() {
+        super();
+
+        console.log('constructor')
+    }
+
+
+
+
+    render() { 
+
+
+        return (
+            <div>
+                <h2>My Class Component</h2>
+
+            </div>
+        )
+    }
+}
+
+
+export default MyClassComponent;
---
+++ src/functionComponent/index.js
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+
+
+function MyFunctionComponent(){
+
+        return(
+        <div>
+            <h2>My Function Component</h2>
+
+        </div>
+    )
+
+}
+export default MyFunctionComponent;
