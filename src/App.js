d412e1b09859b64f8b98b2576710b10a2ee56cca
Shushan04 <ash118817@gmail.com>
2023-07-23 13:43:49 +0400

    Update folders

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import MyClassComponent from './classComponent';
-import MyFunctionComponent from './functionComponent';
+import Login from './view/pages/login';
 
 function App() {
   return (
     <div className="App">
-
+      <Login/>
+ 
     </div>
   );
 }
