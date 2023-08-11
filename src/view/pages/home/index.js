356b17f3a9d233ba88067268fac3ffbc0775a66e
Shushan04 <ash118817@gmail.com>
2023-08-11 21:37:20 +0400

    create home page

--- src/view/pages/home/index.js
+++ src/view/pages/home/index.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { 
+    AppBar,
+    Toolbar,
+    Typography,
+    Button,
+    Box
+} from '@mui/material';
 
+const Navbar = () => {
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    Admin App
+                </Typography>
+                <Box>
+                    <Link to="/login" style={{ textDecoration: 'none'}}>
+                        <Button color='inherit'>
+                            Sign In
+                        </Button>
+                    </Link>
+                    <Link to="/register" style={{ textDecoration: 'none' }}>
+                        <Button color="inherit">
+                            Sign Up
+                        </Button>
+                    </Link>
+                </Box>
+            </Toolbar>
+        </AppBar>
+    );
+};
 
 const Home = () => {
     return (
         <div>
-            
+            <Navbar />
 
         </div>
     );
