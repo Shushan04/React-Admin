12673b946e138427af37386b86d82761684524bd
Shushan04 <132073763+Shushan04@users.noreply.github.com>
2023-08-28 20:39:13 +0400

    delete comment

--- src/App.js
+++ src/App.js
@@ -1,31 +1,3 @@
-// import React from 'react';
-// import Login from './view/pages/login';
-// import Register from './view/pages/register';
-// import {BrowserRouter, Route, Routes} from 'react-router-dom';
-
-
-// function App() {
-
-
-
-//   return (
-//     <div className="App">
-
-//       <BrowserRouter>
-//           <Routes>
-//               <Route path='/login' element={<Login/>}/>
-//               <Route path='/register' element={<Register />} />
-//           </Routes>
-//       </BrowserRouter>
- 
-//     </div>
-//   );
-// }
-
-
-// export default App;
-
-
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './view/pages/home';
